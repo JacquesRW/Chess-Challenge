e8719fa9320c940d7ff6c72aa835cda4a6b6fc49
Archishmaan <archishou@gmail.com>
2023-07-21 12:55:24 -0400

    startup

--- Chess-Challenge/src/Framework/Application/Core/Program.cs
+++ Chess-Challenge/src/Framework/Application/Core/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using ChessChallenge.API;
 
 namespace ChessChallenge.Application {
     static class Program {
         public static void Main() {
-            Console.WriteLine("hi");
+            String initial_pos = "PLACEHOLDER";
+            MyBot bot = new MyBot();
+            Chess.Board fen_board = new Chess.Board();
+            fen_board.LoadPosition(initial_pos);
+            
+            Board bot_board = new Board(fen_board);
+            Timer timer = new Timer(1000);
+            bot.Think(bot_board, timer);
         }
     }
 }
